940c542f3ae6bbbbda82cfe9d9c902f8b96e4f79
Fllixxx <flixh10@gmail.com>
2024-01-15 01:02:38 +0100

    Started inputting the points into pairs

--- PointSortAlgorithm/Main.cpp
+++ PointSortAlgorithm/Main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <utility>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 constexpr auto DEBUG_MODE = true;
+
+template <typename  T>
+void printPair(std::pair<T, T>& pair)
+{
+	std::cout << pair.first() << " " << pair.second() << std::endl;
+}
+
+void printPointPairs(std::vector<std::pair<float, float>> pointPairs)
+{
+	for (std::pair<float, float> pair : pointPairs)
+	{
+		std::cout << pair.first() << " " << pair.second() << std::endl;
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -20,10 +36,27 @@ int main(int argc, char* argv[])
 	std::ifstream inputFile{ argv[1] };
 	if (DEBUG_MODE)
 		std::cout << std::boolalpha << "The File is open: " << inputFile.is_open() << std::endl;
-
 	//input data to a std::pair<float, float>
+	std::string input;
+	std::vector<std::pair<float, float>> pointPairs;
+	while (std::getline(inputFile, input))
+	{
+		std::stringstream inLine{ input };
+		std::string x;
+		std::string y;
+		if (inLine >> x >> y)
+		{
+			std::pair<float, float> p(std::stof(x), std::stof(y));
+			pointPairs.push_back(p);
+			if (DEBUG_MODE)
+				printPair(p);
+				//std::cout << "x=" << x << " | y=" << y << std::endl;
+		}
+	}
 
 	inputFile.close();
+
+	pointPairs.reserve();
 
 	return 0;
 }
